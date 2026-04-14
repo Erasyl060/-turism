e6cf1d9ab94f082092db9529da78b3ce2387983c
Erasyl060 <jhegitaev2006@gmail.com>
2026-04-14 19:27:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-// SPA NAVIGATION (FIX)
-function show(id){
-  document.querySelectorAll(".page").forEach(p=>{
-    p.classList.remove("active");
-  });
-
-  document.getElementById(id).classList.add("active");
-}
-
-// MODAL FIX
 const modal = document.getElementById("modal");
 const openBtn = document.getElementById("openModal");
 const closeBtn = document.getElementById("close");
 
-openBtn.onclick = function(){
+const title = document.getElementById("title");
+const switchBtn = document.getElementById("switch");
+
+let isLogin = true;
+
+// OPEN
+openBtn.onclick = () => {
   modal.style.display = "block";
 };
 
-closeBtn.onclick = function(){
+// CLOSE
+closeBtn.onclick = () => {
   modal.style.display = "none";
 };
 
-window.onclick = function(e){
-  if(e.target == modal){
-    modal.style.display = "none";
+window.onclick = (e) => {
+  if (e.target == modal) modal.style.display = "none";
+};
+
+// SWITCH LOGIN / REGISTER
+switchBtn.onclick = () => {
+  isLogin = !isLogin;
+
+  if (isLogin) {
+    title.innerText = "Кіру";
+    switchBtn.innerText = "Тіркелу жасау";
+  } else {
+    title.innerText = "Тіркелу";
+    switchBtn.innerText = "Кіру";
   }
 };
