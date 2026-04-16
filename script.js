11d58310db25877b4828d6a24d420eed671db20a
Erasyl060 <jhegitaev2006@gmail.com>
2026-04-16 12:00:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-// МОДАЛ
+function toggleMenu() {
+  let menu = document.getElementById("menu");
+  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+}
+
+function showSection(id) {
+  document.querySelectorAll("section").forEach(sec => sec.classList.remove("active"));
+  document.getElementById(id).classList.add("active");
+}
+
 function openModal() {
   document.getElementById("modal").style.display = "block";
 }
@@ -11,20 +20,14 @@ function outsideClick(e) {
   if (e.target.id === "modal") closeModal();
 }
 
-// БУРГЕР МЕНЮ
-function toggleMenu() {
-  let menu = document.getElementById("menu");
-  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
-}
+let images = [
+  "images/burabay.jpg",
+  "images/charyn.jpg",
+  "images/kolsai.jpg",
+  "images/astana.jpg",
+  "images/almaty.jpg"
+];
 
-// SPA
-function showSection(id) {
-  document.querySelectorAll("section").forEach(sec => sec.classList.remove("active"));
-  document.getElementById(id).classList.add("active");
-}
-
-// СЛАЙДЕР
-let images = ["images/1.jpg", "images/2.jpg", "images/3.jpg"];
 let index = 0;
 
 function nextSlide() {
@@ -37,18 +40,15 @@ function prevSlide() {
   document.getElementById("slide").src = images[index];
 }
 
-// АВТО
 setInterval(nextSlide, 3000);
 
-// DARK MODE
 function toggleDark() {
   document.body.classList.toggle("dark");
 }
 
-// LOCAL STORAGE
 function saveData(e) {
   e.preventDefault();
   let name = document.getElementById("name").value;
-  localStorage.setItem("name", name);
+  localStorage.setItem("user", name);
   alert("Сақталды!");
 }
